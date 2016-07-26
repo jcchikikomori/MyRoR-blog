0f4553cd0c479864ef9f240c5f047695922ded33
jccultima123 <johncyrillcorsanes@gmail.com>
2016-07-26 15:33:27 +0800

    Add some info

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	belongs_to :user
+	belongs_to :user #, :foreign_key => 'user_name', :primary_key => 'user_name'
 	has_many :comments
 	validates :title, presence: true, length: { maximum: 50 }
 	validates :post, presence: true
