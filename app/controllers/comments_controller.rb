7a072f0762ea58c7a5063fac0c8f6f6dfaaad136
jccultima123 <johncyrillcorsanes@gmail.com>
2016-07-26 17:02:21 +0800

    [UPDATE] Delete Comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,16 @@ class CommentsController < ApplicationController
 		@comment = @article.comments.create(comment_params)
 		redirect_to article_path(@article)
 	end
-	private
+	def destroy
+		@article = Article.find(params[:article_id])
+		@comment = @article.comments.find(params[:id])
+		@comment.destroy
+	    respond_to do |format|
+	      format.html { redirect_to article_path(@article), notice: 'Comment was successfully destroyed.' }
+	      format.json { head :no_content }
+	    end
+	end
+	private # WARN: functions will be private starting this line
 	def comment_params
 		params.require(:comment).permit(:commenter, :body)
 	end
